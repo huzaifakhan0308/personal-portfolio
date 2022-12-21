576e770db21bb5719301327db223b2bd069d4299
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2022-12-21 20:47:42 +0500

    add popups for desktop and mobile

--- index.js
+++ index.js
@@ -16,3 +16,56 @@ mobileMenu.addEventListener('click', ((e) => {
     mobileMenu.classList.remove('mobile-menu-appear');
   }
 }));
+
+const storeData = [
+  {
+    name: 'Tonic',
+    imagesrc: 'assets/images/project1 img.svg',
+  },
+  {
+    name: 'Multi-post',
+    imagesrc: 'assets/images/project2 img.svg',
+  },
+  {
+    name: 'Tonic',
+    imagesrc: 'assets/images/project3 img.svg',
+
+  },
+  {
+    name: 'Multi-post',
+    imagesrc: 'assets/images/project4 img.svg',
+
+  },
+];
+
+const popups = document.querySelector('.popups');
+const popupsProjectImage = document.querySelector('.popups-project-img');
+const popupProjectTitle = document.querySelector('.popup-project-title');
+
+function popupsDisappearButton() {
+  popups.style.display = 'none';
+}
+
+function projectViewButton1() {
+  popups.style.display = 'flex';
+  popupsProjectImage.src = storeData[0].imagesrc;
+  popupProjectTitle.innerHTML = storeData[0].name;
+}
+
+function projectViewButton2() {
+  popups.style.display = 'flex';
+  popupsProjectImage.src = storeData[1].imagesrc;
+  popupProjectTitle.innerHTML = storeData[1].name;
+}
+
+function projectViewButton3() {
+  popups.style.display = 'flex';
+  popupsProjectImage.src = storeData[2].imagesrc;
+  popupProjectTitle.innerHTML = storeData[2].name;
+}
+
+function projectViewButton4() {
+  popups.style.display = 'flex';
+  popupsProjectImage.src = storeData[3].imagesrc;
+  popupProjectTitle.innerHTML = storeData[3].name;
+}
